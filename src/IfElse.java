52cc08c69abbff89a675df1b32557285dce0bac9
unknown <filipdjurdjevac12356@gmail.com>
2024-01-17 09:58:39 +0100

    first commit

---
+++ src/IfElse.java
@@ -0,0 +1,25 @@
+public class IfElse {
+
+	public static void main(String[] args) {
+		int age = 25;
+		if(age > 0)
+		{
+			if( age > 16 )
+			{
+				System.out.println("Welcome!");
+			}
+			else
+			{
+				System.out.println("Too Young !");
+			}
+			
+		}
+		else 
+		{
+			System.out.println("Error");
+		}
+		// Outputs "Welcome!"
+
+	}
+
+}
